334708d5c6127d4a867d3cb02db376bb32a72f96
Башкатов <bashkat.off.13@gmail.com>
2023-03-14 14:16:35 +0500

    Написал перебор массива строк

--- Program.cs
+++ Program.cs
@@ -9,8 +9,20 @@ for (int i = 0; i < size; i++)
     Console.Write("Введите строку №{0}:\r\n    ", i + 1);
     arrString[i] = Console.ReadLine();
 }
+Console.WriteLine();
 Console.WriteLine("Вы ввели следующие строки: ");
 for (int i = 0; i < size; i++)
 {
     Console.WriteLine(arrString[i]);
 }
+var result = new string[arrString.Length];
+var count = 0;
+foreach (var value in arrString)
+{
+    if (value.Length <= 3)
+    {
+        result[count] = value;
+        count++;
+    }
+}
+Console.WriteLine();
