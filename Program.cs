78f7d7e11022b1abae776301f817bf3e96ba82e7
Башкатов <bashkat.off.13@gmail.com>
2023-03-14 13:56:09 +0500

    Объявил массив строк

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Сколько элементов Вы хотите ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrString = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+    arrString[i] = Console.ReadLine();
+}
+Console.WriteLine("Вы ввели следующие строки: ");
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine(arrString[i]);
+}
